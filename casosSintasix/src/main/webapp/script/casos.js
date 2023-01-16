77b730d1b1cf649b600a2f8f6a2f3fe136651bd5
DESKTOP-FM9L16J\csi22_ejuw1no <david.rodriguez.alonso10@gmail.com>
2023-01-16 12:58:27 +0100

    Casos principales

---
+++ casosSintasix/src/main/webapp/script/casos.js
@@ -0,0 +1,55 @@
+//CASO 1
+function caso1(){
+var a = 5;
+var b = 10;
+
+
+if (a === 5) {
+  let a = 4;
+  var b = 1;
+
+  
+
+document.write(a);
+  
+
+document.write(b);
+}
+
+document.write(a);
+document.write(b);
+
+}
+//CASO 2
+function caso2(){
+for (let i = 0; i<10; i++) {
+  
+
+document.write(i);
+}
+
+document.write(i);
+
+}
+//CASO 3
+function caso3(){
+var x = 5;
+var y = 0;
+
+if (true) {
+  let x=x+10;
+  let y=12;
+
+document.write(x+y);
+}
+
+document.write(x + y);
+
+}
+//CASO 4
+function caso4(){
+ var a=5;
+//let(a = 6) 
+document.write(a);
+document.write(a);
+}
